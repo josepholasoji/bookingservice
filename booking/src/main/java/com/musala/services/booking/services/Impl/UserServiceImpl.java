1e540fbf96242e34fc3a8f4034089177a83f633e
Soji Joseph <josepholasoji@gmail.com>
2024-08-04 12:51:14 +0100

    with migration

--- booking/src/main/java/com/musala/services/booking/services/Impl/UserServiceImpl.java
+++ booking/src/main/java/com/musala/services/booking/services/Impl/UserServiceImpl.java
@@ -1,12 +1,7 @@
 package com.musala.services.booking.services.Impl;
 
 import java.util.*;
-
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +11,6 @@ import com.musala.services.booking.models.Event;
 import com.musala.services.booking.models.User;
 import com.musala.services.booking.repository.UserDao;
 import com.musala.services.booking.services.UserService;
-import org.springframework.core.io.Resource;
 import jakarta.annotation.PostConstruct;
 import jakarta.transaction.Transactional;
 
@@ -29,9 +23,6 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    @Autowired
-    private DataSource dataSource;
-
     private static final String ADMIN_EMAIL = "anadmi@email.com";
     private static final String ADMIN_PASSWORD = "anadminpassword";
     private static final String ADMIN_NAME = "Admin TestAccount";
@@ -41,22 +32,6 @@ public class UserServiceImpl implements UserService {
     // We need to run this like this to ensure that the admin password is hashed in the db
     @PostConstruct
     public void onStartup() {
-
-        // Load the databases 
-        Resource resource = new ClassPathResource("migration/databases.sql");
-        ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator(resource);
-        databasePopulator.execute(dataSource);
-
-        // Load the stored procedures - events
-        resource = new ClassPathResource("migration/procedures/events.sql");
-        databasePopulator = new ResourceDatabasePopulator(resource);
-        databasePopulator.execute(dataSource);
-
-        // Load the stored procedures - users
-        resource = new ClassPathResource("migration/procedures/users.sql");
-        databasePopulator = new ResourceDatabasePopulator(resource);
-        databasePopulator.execute(dataSource);
-
        if(this.getUserByEmail(ADMIN_EMAIL) == null) {
             new User(ADMIN_NAME, ADMIN_EMAIL, ADMIN_PASSWORD, ADMIN_ROLE);
        }
--- migration/tables/bookings.sql
+++
@@ -1,9 +0,0 @@
--- Active: 1722204061621@@127.0.0.1@3306@booking
--- create a table of user event booking
-CREATE TABLE bookings(
-    id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
-    user_id int NOT NULL COMMENT 'User ID',
-    event_id int NOT NULL COMMENT 'Event ID',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT 'Created at',
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Updated at'
-) COMMENT 'User Event Booking Table';
